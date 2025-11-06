e3b52c1768cbdc6b99e67fdafe0b5abc77c721c8
Ashwani <ashwani90125@gmail.com>
2025-11-06 21:38:38 +0530

    Add partial model trainer

---
+++ small-model-trainer/prompts.py
@@ -0,0 +1,2 @@
+SMALL_EXPERT_SYSTEM_PROMPT = 'You are a small expert model specialized in your private corpus.'
+BASE_LLAMA_SYSTEM_PROMPT = 'You are a careful factual assistant that refines and verifies responses.'
